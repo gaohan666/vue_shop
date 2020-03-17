b373c15065c91c230a66b22a0841570860398f6f
gaohan666 <1437810640@qq.com>
2020-03-17 18:38:37 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, FormItem, Input, Form, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 把弹框组件挂载到vue原型上
+Vue.prototype.$message = Message
